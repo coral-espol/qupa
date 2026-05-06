6c3201d57a502b351dfbd8b300f1903318947ba6
David Torres <109327600+davatorr1009@users.noreply.github.com>
2026-05-06 12:00:45 -0500

    Update ROS2 environment script for Pi Zero W2
    Add cyclone dds rwm 
    Eliminate static peers due to not working on WSL2

--- ros2_env_robot.bash
+++ ros2_env_robot.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# ── ROS2 Jazzy — Robot (192.168.0.120) ───────────────────────────────────────
+# ── ROS2 Jazzy — Robot (Pi Zero W2) ─────────────────────────────────────────
 # Usage: source ros2_env_robot.bash
 
 source /opt/ros/jazzy/setup.bash
 
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_DOMAIN_ID=0
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
-export ROS_STATIC_PEERS=192.168.0.111
 
-echo "[ROS2 ROBOT] Jazzy sourced — IP=${ROS_IP}, DOMAIN_ID=${ROS_DOMAIN_ID}"
+echo "[ROS2 ROBOT] Jazzy sourced — DOMAIN_ID=${ROS_DOMAIN_ID} | RMW=CycloneDDS"
