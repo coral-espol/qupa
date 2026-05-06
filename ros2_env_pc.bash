51a3646e1c37d0e1291d0854f5fcb19dc5c128b2
David Torres <davatorr@espol.edu.ec>
2026-05-05 21:51:15 -0500

    add fastdds configurations

--- ros2_env_pc.bash
+++ ros2_env_pc.bash
@@ -19,6 +19,8 @@ export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
 #   192.168.0.120  — qupa_3A  (Raspberry Pi Zero W2, robot 1)
 #   192.168.0.105  — qupa_3B  (Raspberry Pi Zero W2, robot 2)
 #   192.168.0.122  — qupa_3C  (Raspberry Pi Zero W2, robot 3)
-export ROS_STATIC_PEERS="192.168.0.120;192.168.0.105;192.168.0.122;192.168.0.114"
+export ROS_STATIC_PEERS="192.168.0.102;192.168.0.120;192.168.0.105;192.168.0.122;192.168.0.114"
+
+export FASTRTPS_DEFAULT_PROFILES_FILE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/fastdds_pc.xml"
 
 echo "[ROS2 PC/WSL2] Jazzy sourced — IP=${ROS_IP}, DOMAIN_ID=${ROS_DOMAIN_ID}"
--- ros2_env_robot.bash
+++ ros2_env_robot.bash
@@ -8,4 +8,6 @@ export ROS_DOMAIN_ID=0
 export ROS_AUTOMATIC_DISCOVERY_RANGE=SUBNET
 export ROS_STATIC_PEERS=192.168.0.111
 
+export FASTRTPS_DEFAULT_PROFILES_FILE=~/qupa_ws/src/qupa/fastdds_robot.xml
+
 echo "[ROS2 ROBOT] Jazzy sourced — IP=${ROS_IP}, DOMAIN_ID=${ROS_DOMAIN_ID}"
