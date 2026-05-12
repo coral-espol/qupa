3b3594351ff3b4ed3b57eeadef844e9bf0e760ba
user_coral <user_coral@outlook.com>
2026-05-12 14:45:30 -0500

    add namespace for led

--- qupa_hardware/qupa_hardware/gpio_led_node.py
+++ qupa_hardware/qupa_hardware/gpio_led_node.py
@@ -43,7 +43,7 @@ class GpioLedNode(Node):
             except Exception as e:
                 self.get_logger().error(f'UV LED init failed on pin {pin}: {e}')
 
-        self.create_service(SetBool, 'set', self._set_cb)
+        self.create_service(SetBool, '~/set', self._set_cb)
         self.get_logger().info(f'UV LED service ready on {self.get_namespace()}/uv_led/set')
 
     def _set_cb(self, request: SetBool.Request,
